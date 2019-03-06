d296c52b66412fed6b89e72d8c9816b63f64baaa
Kristie <33424744+kristiechan@users.noreply.github.com>
2019-03-06 22:58:12 +0800

    Add files via upload

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,5 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var point_1 = require("./point");
+var like_component_1 = require("./like.component");
 var point = new point_1.Point(1, 2);
 point.draw();
+var component = new like_component_1.LikeComponent(10, true);
+component.onClick();
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,4 +1,6 @@
-import { Point } from './point'
- 
+import { Point } from './point';
+import { LikeComponent } from './like.component';
 let point = new Point(1,2);
 point.draw();
+let component = new LikeComponent(10,true);
+component.onClick();
